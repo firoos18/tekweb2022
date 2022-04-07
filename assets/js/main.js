c2d59ffe3e53d70d64443a830c3a574fc574a7e5
firoos18 <itsfiroos@gmail.com>
2022-04-07 20:49:11 +0700

    first vue commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//Start of VueJS Script
+// let app = Vue.createApp({
+//     data : function() {
+//       return {
+//         title : 'Article #1'
+//       }
+//     }
+//   })
+//   app.mount('#title');
+
+// console.log("Hello World")
+//End of VueJS Script
