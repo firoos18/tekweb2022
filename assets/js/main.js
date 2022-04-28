c1af4a143cdb1935ea94a7411ab875625e2c7210
firoos18 <itsfiroos@gmail.com>
2022-04-28 15:17:50 +0700

    renaming markdown file

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,44 +20,44 @@ Vue.createApp({
             console.log(error);
           })
         },
-        getMarkdownLink(){
-          var converter = new showdown.Converter();
-        axios  
-            .get(
-            "https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/json/article.json"
-          )
-          .then((res) => {
-              for(var i of Object.keys(res.data)){
-                  this.link = res.data[i].markdown;
-                  console.log(this.link);
-              }
-          })
-          .catch((error) => {
-            console.log(error);
-          })
-        },
-        async toTheMarkdown(){
-            var converter = new showdown.Converter();
-            const config = {
-                method : 'get',
-                url : 'https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/markdown/DummyArticle.md'
-            }
+        // getMarkdownLink(){
+        //   var converter = new showdown.Converter();
+        // axios  
+        //     .get(
+        //     "https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/json/article.json"
+        //   )
+        //   .then((res) => {
+        //       for(var i of Object.keys(res.data)){
+        //           this.link = res.data[i].markdown;
+        //           console.log(this.link);
+        //       }
+        //   })
+        //   .catch((error) => {
+        //     console.log(error);
+        //   })
+        // },
+        // async toTheMarkdown(){
+        //     var converter = new showdown.Converter();
+        //     const config = {
+        //         method : 'get',
+        //         url : 'https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/markdown/DummyArticle.md'
+        //     }
 
-            let res = await axios(config);
-            var html = converter.makeHtml(res.data);
-            this.markdownArticle = html;
+        //     let res = await axios(config);
+        //     var html = converter.makeHtml(res.data);
+        //     this.markdownArticle = html;
 
-            if(res.status === 200) {
-              console.log('Data Retrieved')
-            }
-            // console.log(this.markdownArticle);
-            // console.log(res.status);
+        //     if(res.status === 200) {
+        //       console.log('Data Retrieved')
+        //     }
+        //     // console.log(this.markdownArticle);
+        //     // console.log(res.status);
 
-        }  
+        // }  
     },
     beforeMount() {
-      this.toTheMarkdown()
       this.getJsonData()
-      this.getMarkdownLink()
+      // this.toTheMarkdown()
+      // this.getMarkdownLink()
     },
   }).mount("#app");
