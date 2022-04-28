796bde3acf3cf4a7f81615cc4fc22e55110e3bac
firoos18 <itsfiroos@gmail.com>
2022-04-28 21:24:31 +0700

    getting markdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,63 +1,26 @@
-Vue.createApp({
-    data() {
-      return {       
-        markdownArticle : null,
-        jsondata : {},
-        link : {}
-      };
-    },
-    methods: {
-        getJsonData(){
-        axios  
-            .get(
-            "https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/json/article.json"
-          )
-          .then((res) => {
-            // console.log(res.data);
-            this.jsondata = res.data;
-          })
-          .catch((error) => {
-            console.log(error);
-          })
-        },
-        // getMarkdownLink(){
-        //   var converter = new showdown.Converter();
-        // axios  
-        //     .get(
-        //     "https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/json/article.json"
-        //   )
-        //   .then((res) => {
-        //       for(var i of Object.keys(res.data)){
-        //           this.link = res.data[i].markdown;
-        //           console.log(this.link);
-        //       }
-        //   })
-        //   .catch((error) => {
-        //     console.log(error);
-        //   })
-        // },
-        // async toTheMarkdown(){
-        //     var converter = new showdown.Converter();
-        //     const config = {
-        //         method : 'get',
-        //         url : 'https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/markdown/DummyArticle.md'
-        //     }
-
-        //     let res = await axios(config);
-        //     var html = converter.makeHtml(res.data);
-        //     this.markdownArticle = html;
-
-        //     if(res.status === 200) {
-        //       console.log('Data Retrieved')
-        //     }
-        //     // console.log(this.markdownArticle);
-        //     // console.log(res.status);
-
-        // }  
-    },
-    beforeMount() {
-      this.getJsonData()
-      // this.toTheMarkdown()
-      // this.getMarkdownLink()
-    },
-  }).mount("#app");
+let app = Vue.createApp({
+  data() {
+    return {
+      articles: {},
+      markdown: null
+    }
+  },
+  methods: {
+    getArticleData() {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/json/article.json"
+        )
+        .then((res) => {
+          this.articles = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  },
+  beforeMount() {
+    this.getArticleData()
+  }
+})
+app.mount('#app');
---
+++ assets/js/read.js
@@ -0,0 +1,29 @@
+let app = Vue.createApp({
+    data() {
+      return {
+        markdown: null
+      }
+    },
+    methods: {
+        getMarkdownFile(){
+            var converter = new showdown.Converter();
+            var url = window.location.href
+            let fileName = url.slice(45,57)
+            // console.log(fileName)
+
+            axios
+                .get(
+                    "https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/markdown/" + fileName
+                )
+                .then((res) => {
+                    var html = converter.makeHtml(res.data);           
+                    this.markdown = html;
+                    // console.log(html);
+                })
+        }
+    },
+    beforeMount() {
+      this.getMarkdownFile()
+    }
+  })
+  app.mount('#app');
