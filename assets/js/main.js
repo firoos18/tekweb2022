9e1f03d2cf7529b58dd89b94d72a7c6eaa029998
firoos18 <itsfiroos@gmail.com>
2022-07-14 20:18:38 +0700

    API implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,47 @@
+let apiPoints = "https://myapi.veerouze.my.id/index.php/"
+
 let app = Vue.createApp({
   data() {
     return {
       articles: {},
+      biodata : {},
+      social : {},
       markdown: null
     }
   },
   methods: {
     getArticleData() {
       axios
-        .get(
-          "https://raw.githubusercontent.com/firoos18/tekweb2022/main/assets/json/article.json"
-        )
+        .get(apiPoints + "article")
         .then((res) => {
           this.articles = res.data;
+          // console.log(this.articles);
         })
         .catch((error) => {
           console.log(error);
         });
-    }
+      },
+      getBiodata() {
+        axios
+          .get(apiPoints + "biodata/1")
+          .then((res) => {
+            this.biodata = res.data;
+            // console.log(this.biodata);
+          })
+      },
+      getSocial() {
+        axios
+          .get(apiPoints + "social/1")
+          .then((res) => {
+            this.social = res.data;
+            console.log(this.social);
+          })
+      }
   },
   beforeMount() {
-    this.getArticleData()
+    this.getArticleData(),
+    this.getBiodata(),
+    this.getSocial()
   }
 })
 app.mount('#app');
